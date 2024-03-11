c8c92c6ab88935686f7d70d214017e4c74c21ff0
Olivia Stewart <liv.stewart2004@gmail.com>
2024-03-11 19:27:09 +1100

    added message button

--- server.py
+++ server.py
@@ -1,16 +1,24 @@
 from socket import *
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverPort = input("Enter port number: ")
 
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverPort = 12000
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print('The server is ready to receive')
 while True:
     connectionSocket, addr = serverSocket.accept()
     try:
+
         message = connectionSocket.recv(1024)
-        filename = message.split()[1]
-        f = open(filename[1:])
+        messageRawContent = message.split()[1]
+        raw = messageRawContent.decode('utf-8')
+        print(raw)
+        if '?' in raw:
+            connectionSocket.send('HTTP/1.1 200 OK\n'.encode())
+            continue
+       
+        f = open(messageRawContent[1:])
+
         outputData = f.read()
         connectionSocket.send('HTTP/1.1 200 OK\n'.encode())
         connectionSocket.send('Content-Type: text/html\n'.encode())
@@ -19,7 +27,11 @@ while True:
             connectionSocket.send(outputData[i].encode())
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
+
     except IOError:
+
         header = 'HTTP/1.1 404 Not Found\n\n'
+
         connectionSocket.send(header.encode())
+
         connectionSocket.close()
